70d86bd19b4eaa2b4df2cfcebb406903f78734bb
joey-tschirpy <joey.tschirpy@gmail.com>
2019-01-18 19:25:16 +1000

    - Import tank assets - Create Player - Create Player Data

---
+++ MK Test Project - Shooter/Assets/Scripts/Scriptable Objects/PlayerData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName ="Player Data")]
+public class PlayerData : ScriptableObject {
+    [SerializeField] float health;
+    [SerializeField] float healthMax;
+    [SerializeField] float moveSpeed;
+
+    public float Health()
+    {
+        return health;
+    }
+
+    public float HealthMax()
+    {
+        return healthMax;
+    }
+
+    public float MoveSpeed()
+    {
+        return moveSpeed;
+    }
+}
